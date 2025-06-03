23140e0bb5c7ff30ffb63c7f3ef6656aa1897451
sajib-hosen <sajib.201h@gmail.com>
2025-06-03 21:35:15 +0600

    fix: fix the contact link page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,39 +5,44 @@ import Link from "next/link";
 import React from "react";
 
 export const metadata = {
-  title: "Contact",
+    title: "Contact",
 };
 
 export default function BlogPg() {
-  return (
-    <div className=" relative content-w space-y-8 mt-8 p-4">
-      <p className="max-w-[500px] mx-auto">
-        Feel free to reach out to me through any of these platforms for
-        inquiries, collaborations, or just to say hello.
-      </p>
+    return (
+        <div className=" relative content-w space-y-8 mt-8 p-4">
+            <p className="max-w-[500px] mx-auto">
+                Feel free to reach out to me through any of these platforms for
+                inquiries, collaborations, or just to say hello.
+            </p>
 
-      <div className=" max-w-[500px] mx-auto space-y-4 ">
-        {social_links_data.length
-          ? social_links_data.map((e) => (
-              <div
-                key={e.title}
-                className=" border hover:border-gray-500 transition-all duration-300 ease-in-out rounded"
-              >
-                <Link className="flex items-center" href={e.link} key={e.title}>
-                  <div className="w-14 h-14 flex justify-center items-center">
-                    {e.icon}
-                  </div>
-                  <div className=" text-gray-400">
-                    <p>{e.short_url}</p>
-                    <p className=" font-bold">{e.title}</p>
-                  </div>
-                </Link>
-              </div>
-            ))
-          : null}
-      </div>
+            <div className=" max-w-[500px] mx-auto space-y-4 ">
+                {social_links_data.length
+                    ? social_links_data.map((e) => (
+                          <div
+                              key={e.title}
+                              className=" border hover:border-gray-500 transition-all duration-300 ease-in-out rounded"
+                          >
+                              <Link
+                                  target="_blank"
+                                  className="flex items-center"
+                                  href={e.link}
+                                  key={e.title}
+                              >
+                                  <div className="w-14 h-14 flex justify-center items-center">
+                                      {e.icon}
+                                  </div>
+                                  <div className=" text-gray-400">
+                                      <p>{e.short_url}</p>
+                                      <p className=" font-bold">{e.title}</p>
+                                  </div>
+                              </Link>
+                          </div>
+                      ))
+                    : null}
+            </div>
 
-      <TimeStamp />
-    </div>
-  );
+            <TimeStamp />
+        </div>
+    );
 }
