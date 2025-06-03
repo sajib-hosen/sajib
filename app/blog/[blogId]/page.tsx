a795e67747d8bbd58e0f80721d4e1072d602b8ec
sajib-hosen <sajib.201h@gmail.com>
2025-06-03 22:37:40 +0600

    update blog details page

--- app/blog/[blogId]/page.tsx
+++ app/blog/[blogId]/page.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const BlogPage = () => {
-    return <div>This is blog page</div>;
+    return (
+        <div>
+            <h1>This is blog page</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+                corrupti harum omnis ducimus minima beatae et maxime? Ratione
+                harum accusamus dolorem culpa deleniti. Odio ipsum vero, quia
+                harum aspernatur quam.
+            </p>
+        </div>
+    );
 };
 
 export default BlogPage;
